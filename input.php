ee181486db1b2a8bad041bd331f975cce2932f09
Meaownx <saedelta16alifah@gmail.com>
2024-10-27 22:55:02 +0700

    update

--- input.php
+++ input.php
@@ -1,7 +1,7 @@
 <?php
 $kecamatan = $_POST['kecamatan'];
 $longitude = $_POST['longitude'];
-$latitude = $_POST["latitude"];
+$latitude = $_POST['latitude'];
 $luas = $_POST['luas'];
 $jumlah_penduduk = $_POST['jumlah_penduduk'];
 
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 $sql = "INSERT INTO kecamatan (kecamatan, longitude, latitude, luas, jumlah_penduduk)
-VALUES ('$kecamatan', $longitude,$latitude, $luas, $jumlah_penduduk)";
+VALUES ('$kecamatan', '$longitude','$latitude', '$luas', '$jumlah_penduduk')";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
